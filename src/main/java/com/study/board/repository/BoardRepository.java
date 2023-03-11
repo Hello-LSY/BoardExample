3638fe1c49774394d69ec8ae94a3f76c31aeb5cb
Hello-LSY <leesy9520@naver.com>
2023-03-11 19:24:17 +0900

    modify commit

--- src/main/java/com/study/board/repository/BoardRepository.java
+++ src/main/java/com/study/board/repository/BoardRepository.java
@@ -1,9 +1,14 @@
 package com.study.board.repository;
 
 import com.study.board.entity.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BoardRepository extends JpaRepository<Board, Integer> {
+
+    //findBy(칼럼이름)Containing 명령규칙으로 칼럼이름에 키워드가 포함된것을 찾음
+    Page<Board> findByTitleContaining(String searchKeywordm, Pageable pageable);
 }
--- src/main/java/com/study/board/service/BoardService.java
+++ src/main/java/com/study/board/service/BoardService.java
@@ -3,6 +3,8 @@ package com.study.board.service;
 import com.study.board.entity.Board;
 import com.study.board.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -21,7 +23,6 @@ public class BoardService {
 
         //경로지정
         String projectPath = System.getProperty("user.dir") + "\\src\\main\\webapp";
-
         //랜덤 uuid 생성
         UUID uuid = UUID.randomUUID();
 
@@ -37,13 +38,17 @@ public class BoardService {
     }
 
     //게시글 리스트 처리
-    public List<Board> boardList(){
-        return boardRepository.findAll();
+    public Page<Board> boardList(Pageable pageable){
+        return boardRepository.findAll(pageable);
     }
 
     //특정 게시글 불러오기
     public Board boardView(Integer id){
         return boardRepository.findById(id).get();
+    }
+
+    public Page<Board> boardSearchList(String searchKeyword, Pageable pageable){
+        return boardRepository.findByTitleContaining(searchKeyword, pageable);
     }
 
     //특정 게시글 삭제
